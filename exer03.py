e56d070740812bfc332e704af0842bab5db4bec0
Paulo-Ortiz <117416105+Paulo-Ortiz@users.noreply.github.com>
2022-11-03 22:22:41 -0300

    Add files via upload

---
+++ exer03.py
@@ -0,0 +1,13 @@
+#trabalhando com variáveis
+#num1 = 10
+#num2 = 5
+num1 = input('Informe o primeiro valor:')
+num2 = input('Informe o segundo valor:')
+
+print(type(num1))
+
+soma = num1 + num2
+print('A soma é: ', soma)
+
+soma1 = int(num1) + int(num2)
+print('A soma correta é: {}' . format(soma1))
